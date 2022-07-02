89a2db0b2291f6a649acc926a68396b31b04908b
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-02 05:45:45 -0700

    betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,6 @@
 * main - Entry point
 *
 * Return: Always 0 (Success)
-*
 */
 int main(void)
 {
@@ -15,15 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	} else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	} else if (n < 0)	
-		printf("%i is negative\n", n);
-											}
-
+		printf("%d is positive\n", n);
+        else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)	
+		printf("%d is negative\n", n);
 	return (0);
-
 }
