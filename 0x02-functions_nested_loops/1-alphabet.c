01c7093ec57d253208b6cfcb57990f0071d26f8d
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-05 06:28:39 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints alphabet in lowercase
+ *
+ *   * print alphabet a to z
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+
+
+void print_alphabet(void)
+
+{
+
+		char c = 'a';
+
+
+
+			while (c <= 'z')
+
+					{
+
+								_putchar(c);
+
+										c++;
+
+											}
+
+				_putchar('\n');
+
+}
