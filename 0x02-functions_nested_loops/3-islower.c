7915b17a9026e064f0686037c7662ceac45beab2
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-05 07:28:49 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet_x10 - prints alphabet 10 times
+ *
+ *   * No parameter needed
+ *
+ *    * Return: void
+ *
+ *     */
+
+void print_alphabet_x10(void)
+
+{
+
+		int i;
+
+			char c;
+
+
+
+				for (i = 1; i <= 10; i++)
+
+						{
+
+									for (c = 'a'; c <= 'z'; c++)
+
+												{
+
+																_putchar(c);
+
+																		}
+
+											_putchar('\n');
+
+												}
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
