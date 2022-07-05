d2ead62b58ed863a5853dc55d938fed6b0781748
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-05 16:38:41 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *
+ *  * add - adding it up
+ *
+ *   * @a: first integer
+ *
+ *    * @b: second integer
+ *
+ *     *
+ *
+ *      * Return: sum of both integers
+ *
+ *       */
+
+int add(int a, int b)
+
+{
+
+		return (a + b);
+
+}
