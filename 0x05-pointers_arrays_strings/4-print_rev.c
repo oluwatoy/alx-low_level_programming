cd00e4fd0e22daf26b41316b05e062ae8be0f9b9
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-13 05:52:07 -0700

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 
 void print_rev(char *s)
 {
-	int c = 0;
+	int len = 0;
 	int i;
 
 	/* iterate to find length of string and point to last character */
 	while (*s != '\0')
 	{
-		c++;
+		len++;
 		++s;
 	}
 
@@ -21,7 +21,7 @@ void print_rev(char *s)
 	s--;
 
 	/* print string reversed */
-	for (i = c; i >= 0; i--)
+	for (i = len; i >= 0; i--)
 	{
 		_putchar(*s);
 		s--;
