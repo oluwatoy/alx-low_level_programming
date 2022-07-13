9cb3f28a7205f18efb66ee93e3f6032ad1caddef
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-13 04:57:56 -0700

    tputs

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,27 +1,15 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  *
- *  * _puts - prints a string, followed by a new line, to stdout
- *
- *   * @str: string to print
- *
- *    */
-
-
+ */
 
 void _puts(char *str)
-
 {
+	while (*str != '\0')
+		_putchar(*str++);
 
-		while (*str != '\0')
-
-					_putchar(*str++);
-
-
-
-			_putchar('\n');
-
+	_putchar('\n');
 }
