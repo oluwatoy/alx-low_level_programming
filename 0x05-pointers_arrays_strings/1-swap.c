8e2541f3de7663fc6a19bba63b5fbd9ecfac6cc8
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-13 04:15:51 -0700

    initial commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,18 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: integer to swap
- * @b: integer to swap
+ * @a: integer to swap with b
+ *
+ * @b: integer to swap with a
+ *
+ * Return: no return
  */
-
 void swap_int(int *a, int *b)
 {
-	int tmp;
+	int temp_a, temp_b;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp:
+	temp_a = *a;
+	temp_b = *b;
+	*a = temp_b;
+	*b = temp_a;
 }
