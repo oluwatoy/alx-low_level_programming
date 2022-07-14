01d601734a6ea86e4b2887db5bf9ae133918a740
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-14 02:20:00 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,35 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *
- *  * _isalpha - main function
- *
- *   *
- *
- *    * @c: character to compare
- *
- *     *
- *
- *      * Return: 1 if true, 0 if false
- *
- *       *
- *
- *        */
-
-
-
+ * _isalpha - checks if it is an alphabet
+ * @c: The character (parameter) to be checked
+ * Return: 1 if c is letter, returns 0 otherwise
+ */
 int _isalpha(int c)
-
 {
-
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-				{
-
-							return (1);
-
-								}
-
-			return (0);
-
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
