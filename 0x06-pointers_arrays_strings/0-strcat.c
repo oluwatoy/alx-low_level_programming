1dd82893391aca994f091dc4b66f1b010b0be0fd
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-14 05:51:27 -0700

    initial

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strcat - concatenates two strings
@@ -8,18 +9,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
-
-	for (j = 0; dest[j] != '\0'; j++)
-	{}
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[j + i] = src[i]
-	}
-	dest[j + i] = '\0'
-
+	strcat(dest, src);
 	return (dest);
-
 }
