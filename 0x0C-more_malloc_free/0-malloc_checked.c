a3648aaafa48bd184624246e5ee68427d8d04322
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-07-28 09:49:26 -0700

    initial commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,12 +8,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	
-	void *p;
-	
-	p = malloc(b);
-	if (p == NULL)
-		exit (98);
+	void *p = malloc(b);
 
+	if (p == NULL)
+		exit(98);
 	return (p);
+
 }
