747c6f3020cf59214a9f7769809f61ae6bd12598
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-08-01 05:04:05 -0700

    initial commits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,13 @@ struct dog
 };
 
 /**
- * dog_t - Typedef forog *d, char *name, float age, char *owner);
+ * dog_t - Typedef for dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
