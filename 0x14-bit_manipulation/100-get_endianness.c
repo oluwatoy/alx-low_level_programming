3415499db4fc343313696a745680dbf79359653b
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-08-17 12:07:40 -0700

    initial commits

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - detects endian architecture
+ * Return: big endian(0), little endian(1)
+ */
+int get_endianness(void)
+{
+	unsigned int x = 0x76543210;
+	char *c = (char *) &x;
+
+	if (*c == 0x10)
+		return (1);
+	return (0);
+}
