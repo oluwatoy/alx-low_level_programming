51a4d114e2d95ee7c980de87652fc21280fbfe51
Bamidele Emmanuel <107483062+oluwatoy@users.noreply.github.com>
2022-08-22 17:16:55 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,3 +12,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	size_t len;
 
 	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		written = write(fd, text_content, len);
+	}
+	close(fd);
+	if (written == -1)
+		return (-1);
+}
