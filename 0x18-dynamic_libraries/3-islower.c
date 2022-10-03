91b813173e03b78c0db0a5e80358be9aeefd88b1
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2022-10-03 15:02:24 -0700

    library in c

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check if a character is lowercase
+ * @c: Take a character c as argument to be checked
+ * Return:  1 if successful and 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
